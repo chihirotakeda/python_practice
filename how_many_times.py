ce85e5f167dabfa35a9a1631bca5cf77d530d46a
chihirotakeda <chihiro.takeda@gmail.com>
2019-09-14 13:50:05 -0500

    Add functions to repeat a specific string in a word.

---
+++ how_many_times.py
@@ -0,0 +1,10 @@
+def how_many_times(num):
+    return 'Ed{}bit'.format('a' * num)
+
+
+print(how_many_times(5))
+print(how_many_times(15))
+print(how_many_times(10))
+print(how_many_times(23))
+print(how_many_times(1))
+
