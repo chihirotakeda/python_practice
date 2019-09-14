77c6a0d12cc66096af28c98a0dfd8a54fdb3cd8f
chihirotakeda <chihiro.takeda@gmail.com>
2019-09-14 09:42:03 -0500

    Add functions to find the smallest number in the list.

---
+++ smallest_num.py
@@ -0,0 +1,11 @@
+def find_smallest_num(lst):
+    return min(lst)
+
+
+list1 = [34, 15, 88, 2]
+list2 = [34, -345, -1, 100]
+list3 = [0.4356, 0.8795, 0.5435, -0.9999]
+
+print(find_smallest_num(list1))
+print(find_smallest_num(list2))
+print(find_smallest_num(list3))
