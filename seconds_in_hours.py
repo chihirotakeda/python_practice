d386cfa3fd1da4ca162a071aec6e416ede384440
chihirotakeda <chihiro.takeda@gmail.com>
2019-09-23 12:38:13 -0500

    Add another function to convert hours and minutes to seconds.

--- seconds_in_hours.py
+++ seconds_in_hours.py
@@ -7,3 +7,11 @@ print(seconds_in_hours(10))
 print(seconds_in_hours(24))
 
 
+def convert(hours, minutes):
+    return hours * 3600 + minutes * 60
+
+
+print(convert(1, 3))
+print(convert(2, 0))
+print(convert(0, 0))
+
