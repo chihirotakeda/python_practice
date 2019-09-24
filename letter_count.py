d52e870cc82aefbebb1a52dea3963f436d04fe75
chihirotakeda <chihiro.takeda@gmail.com>
2019-09-24 16:44:54 -0500

    Add a function to count a specific letter in a list.

---
+++ letter_count.py
@@ -0,0 +1,14 @@
+def letter_counter(lst, letter):
+    count = 0
+    for i in lst:
+        count = count + i.count(letter)
+    return count
+
+
+print(letter_counter([
+  ["D", "E", "Y", "H", "A", "D"],
+  ["C", "B", "Z", "Y", "J", "K"],
+  ["D", "B", "C", "A", "M", "N"],
+  ["F", "G", "G", "R", "S", "R"],
+  ["V", "X", "H", "A", "S", "S"]
+], "D"))
